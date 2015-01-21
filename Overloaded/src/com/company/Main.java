580379ee5ca9bef897638fa4511ad10e8a257131
E.L <eladlavi@Elads-MacBook-Pro.local>
2015-01-21 20:30:48 +0200

    Car

---
+++ Overloaded/src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Main {
+
+    //public static void aMethod(byte val){System.out.println("byte");}
+    //public static void aMethod(int val){System.out.println("int");}
+    //public static void aMethod(short val){System.out.println("short");}
+    //public static void aMethod(Object val){System.out.println("Object");}
+    //public static void aMethod(String val){System.out.println("String");}
+    public static void aMethod(long val1, int val2){
+        System.out.println("long, int");
+    }
+    public static void aMethod(int val1, long val2){
+        System.out.println("int, long");
+    }
+
+
+    public static void main(String[] args) {
+	    //byte b = 9;
+        //aMethod(b);
+        //aMethod(9);
+        //Integer i = 9;
+        //aMethod(i);
+        //aMethod("9");
+        //aMethod(9L, 10);
+
+    }
+}
---
+++ untitled/src/com/company/Car.java
@@ -0,0 +1,52 @@
+package com.company;
+
+/**
+ * Created by eladlavi on 1/21/15.
+ */
+public class Car {
+    private int model;
+    private String make;
+    private boolean manualGear;
+
+    public Car(int model, String make, boolean manualGear){
+        setModel(model);
+        setMake(make);
+        setManualGear(manualGear);
+    }
+
+    public Car(int model, String make){
+        this(model, make, true);
+    }
+
+    public Car(String make){
+        this(2014, make);
+    }
+
+    public Car(){
+        this("Toyota");
+    }
+
+    public int getModel() {
+        return model;
+    }
+
+    public void setModel(int model) {
+        this.model = model;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public boolean isManualGear() {
+        return manualGear;
+    }
+
+    public void setManualGear(boolean manualGear) {
+        this.manualGear = manualGear;
+    }
+}
