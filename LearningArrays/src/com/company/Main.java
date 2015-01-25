abb05f6e2bf7a25b0c4dafa89022e6ef9c767026
E.L <eladlavi@Elads-MacBook-Pro.local>
2015-01-25 21:07:53 +0200

    Filter Array

--- LearningArrays/src/com/company/Main.java
+++ LearningArrays/src/com/company/Main.java
@@ -3,13 +3,25 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        /*
         //int num = 8;
         MyInt myInt = new MyInt();
         myInt.num = 8;
         stam(myInt);
         System.out.println(myInt.num);
+        */
+        printArray(stam3());
+
+
     }
 
+
+    public static void printArray(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+",");
+        }
+        System.out.println("end");
+    }
     public static double average(int[] numbers){
         int sum = 0;
         for(int i=0;i<numbers.length;i++)
@@ -21,19 +33,44 @@ public class Main {
     // and an int, and will return how many numbers in the array,
     //are greater than the given number
     public static int howManyAbove(int[] numbers, int lowerBound){
-        return 0;
+        int counter = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if(numbers[i]>lowerBound)
+                counter++;
+        }
+        return counter;
     }
 
     public static int max(int[] numbers){
-        return 0;
+        if(numbers!=null && numbers.length>0) {
+            int max = numbers[0];
+            for (int i = 1; i < numbers.length; i++)
+                if (numbers[i] > max)
+                    max = numbers[i];
+            return max;
+        }else{
+            return Integer.MIN_VALUE;
+        }
     }
 
     public static int[] flipArray(int[] numbers){
-        return null;
+        if(numbers==null || numbers.length==0)
+            return null;
+        int[] flipped = new int[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            flipped[i] = numbers[numbers.length - i - 1];
+        }
+        return flipped;
     }
 
-    public static void flipTheArray(int[] numbers){
 
+    public static void flipTheArray(int[] numbers){
+        int temp;
+        for (int i = 0; i < numbers.length / 2; i++) {
+            temp = numbers[i];
+            numbers[i] = numbers[numbers.length-i-1];
+            numbers[numbers.length-i-1] = temp;
+        }
     }
 
     public static void stam(int x){
@@ -48,5 +85,16 @@ public class Main {
     }
 
 
+    public static int[] stam3(){
+        int[] arr = new int[3];
+        arr[0] = 13;
+        arr[1] = 15;
+        arr[2] = 20;
+        return  arr;
+    }
+
+    public static int[] filterArray(int[] numbers, int lowerBound, int upperBound){
+
+    }
 
 }
